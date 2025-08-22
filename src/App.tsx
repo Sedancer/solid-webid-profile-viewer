d783b861fc05c1629ed8b7dae7b064339ab896f9
sedancer <zborpc@gmail.com>
2025-08-22 01:42:33 +0100

    adding some improvements

--- src/App.tsx
+++ src/App.tsx
@@ -29,23 +29,21 @@ export default function App() {
 
             <Header webId={currentWebId} defaultWebId={DEFAULT_WEBID}/>
 
-            <main className="max-w-6xl mx-auto p-4 md:p-6">
+            <main className="w-full p-4 md:p-6">
                 {loading && (
                     <div className="text-center text-gray-600">Loading...</div>
                 )}
 
                 {!loading && error && (
-                    <div className="mb-4">
-                        <Card className="p-4 border border-red-200 bg-red-50 text-red-900">
-                            <div className="font-semibold mb-1">Failed to load profile</div>
-                            <div className="text-sm opacity-90">{error}</div>
-                        </Card>
-                    </div>
+                    <Card className="border border-red-200 bg-red-50 text-red-900">
+                        <div className="font-semibold mb-1">Failed to load profile</div>
+                        <div className="text-sm opacity-90">{error}</div>
+                    </Card>
                 )}
 
                 {!loading && profile && (
-                    <div className="grid grid-cols-1 md:grid-cols-[340px,1fr] gap-6">
-                        <Card className="p-6">
+                    <div className="grid grid-cols-1 xl:grid-cols-2 3xl:grid-cols-3 gap-6" >
+                        <Card>
                             <div className="flex md:flex-row flex-col items-center text-center gap-4">
                                 <div className="w-36 h-36 rounded-2xl overflow-hidden shadow-md bg-slate-100 grid place-items-center">
                                     {profile.avatar ? (
@@ -59,95 +57,85 @@ export default function App() {
                                         <div className="text-5xl">👤</div>
                                     )}
                                 </div>
-                                <div className="text-left flex flex-col">
+                                <div className="flex flex-col max-w-[100%]">
                                     <KeyValue label="Name" value={profile.name}/>
-
+                                    <KeyValue label="WebId" value={currentWebId}/>
                                     {profile.nickname && (
                                         <KeyValue label="Nickname" value={profile.nickname}/>
-                                    )}
-                                    {currentWebId && (
-                                        <KeyValue label="WebId" value={currentWebId}/>
                                     )}
                                 </div>
                             </div>
                         </Card>
-
-                        <div className="space-y-6">
-                            <Card className="p-6">
-                                <div className="space-y-2">
-                                    <div className="font-medium mb-1">Email</div>
-                                    {profile.emails.length ? (
-                                        <ul className="space-y-1">
-                                            {profile.emails.map((email:string) => (
-                                                <li key={email}>
-                                                    <Link href={`mailto:${email}`}> {email} </Link>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    ) : (
-                                        <div className="text-sm text-slate-500">No data</div>
-                                    )}
-                                    {!!profile.tels.length && (<>
-                                        <Divider/>
-                                        <div className="font-medium mb-1">Phone</div>
-                                        <ul className="space-y-1">
-                                            {profile.tels.map((tel:string) => (
-                                                <li key={tel}>
-                                                    <Link href={`tel:${tel}`}>{tel}</Link>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                        </>
-                                    )}
-                                </div>
-                            </Card>
-
-                            {(profile.org || profile.title || profile.homepage || profile.bio) && (
-                                <Card className="p-6 flex md:flex-row flex-col gap-4">
-                                    {profile.org && <KeyValue label="Organization" value={profile.org}/>}
-                                    {profile.title && <KeyValue label="Title" value={profile.title}/>}
-                                    {profile.homepage && (
-                                        <KeyValue
-                                            label="Homepage"
-                                            value={
-                                                <a
-                                                    href={profile.homepage}
-                                                    target="_blank"
-                                                    rel="noreferrer"
-                                                    className="text-indigo-600 hover:underline break-all"
-                                                >
-                                                    {profile.homepage}
-                                                </a>
-                                            }
-                                        />
-                                    )}
-                                    {profile.bio && <KeyValue label="Bio" value={profile.bio}/>}
-                                </Card>
+                        <Card>
+                            <h4 className="font-medium mb-1">Email</h4>
+                            {profile.emails.length ? (
+                                <ul className="space-y-1">
+                                    {profile.emails.map((email:string) => (
+                                        <li key={email}>
+                                            <Link href={`mailto:${email}`}> {email} </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <div className="text-sm text-slate-500">No data</div>
                             )}
-
-
-                            <Card className="p-6">
-                                <div className="text-lg font-semibold mb-3">Connections (foaf:knows)</div>
-                                {profile.knows.length ? (
-                                    <ul className="list-disc pl-6 space-y-1">
-                                        {profile.knows.map((know:string) => (
-                                            <li key={know}>
-                                                <a
-                                                    className="text-indigo-600 hover:underline break-all"
-                                                    href={know}
-                                                    target="_blank"
-                                                    rel="noreferrer"
-                                                >
-                                                    {know}
-                                                </a>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                ) : (
-                                    <div className="text-sm text-slate-500">No data</div>
+                            {!!profile.tels.length && (<>
+                                <Divider/>
+                                <h4 className="font-medium mb-1">Phone</h4>
+                                <ul className="space-y-1">
+                                    {profile.tels.map((tel:string) => (
+                                        <li key={tel}>
+                                            <Link href={`tel:${tel}`}>{tel}</Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                                </>
+                            )}
+                        </Card>
+                        {(profile.org || profile.title || profile.homepage || profile.bio) && (
+                            <Card className="flex flex-col gap-4">
+                                {profile.org && <KeyValue label="Organization" value={profile.org}/>}
+                                {profile.title && <KeyValue label="Title" value={profile.title}/>}
+                                {profile.homepage && (
+                                    <KeyValue
+                                        label="Homepage"
+                                        value={
+                                            <a
+                                                href={profile.homepage}
+                                                target="_blank"
+                                                rel="noreferrer"
+                                                className="text-indigo-600 hover:underline break-all"
+                                            >
+                                                {profile.homepage}
+                                            </a>
+                                        }
+                                    />
                                 )}
+                                {profile.bio && <KeyValue label="Bio" value={profile.bio}/>}
                             </Card>
-                        </div>
+                        )}
+
+                        <Card>
+                            <h4 className="text-lg font-semibold mb-3">Connections (foaf:knows)</h4>
+                            {profile.knows.length ? (
+                                <ul className="list-disc pl-6 space-y-1">
+                                    {profile.knows.map((know:string) => (
+                                        <li key={know}>
+                                            <a
+                                                className="text-indigo-600 hover:underline break-all"
+                                                href={know}
+                                                target="_blank"
+                                                rel="noreferrer"
+                                            >
+                                                {know}
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <div className="text-sm text-slate-500">No data</div>
+                            )}
+                        </Card>
                     </div>
                 )}
             </main>
--- src/components/KeyValue.tsx
+++ src/components/KeyValue.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 const KeyValue = ({label = "", value = "Value missing"}: Props) => {
     return (
-        <div className="relative border border-slate-800 rounded-md p-3 inline-flex items-center gap-2 min-w-[330px] mb-4">
-            <span className="text-gray-400 dark:bg-slate-900 font-medium uppercase text-sm font-semibold absolute top-[-8px] px-[5px]">{label}:</span>
-            <span className="font-medium">{value}</span>
+        <div className="relative border border-slate-800 rounded-md p-3 inline-flex items-center gap-2 max-w-[100%] min-w-[268px] mb-4">
+            <span className="text-gray-400 dark:bg-slate-900 uppercase text-sm font-semibold absolute top-[-8px] px-[6px] left-[3px]">{label}:</span>
+            <span className="font-medium max-w-[100%] overflow-x-hidden">{value}</span>
         </div>
     );
 };
